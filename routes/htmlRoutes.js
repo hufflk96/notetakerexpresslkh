6c8500664bac26fee7184b6a4080f57629cc5f94
hufflk96 <118147274+hufflk96@users.noreply.github.com>
2023-05-29 08:02:03 -0400

    Update htmlRoutes.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,12 +1,8 @@
 const router = require('express').Router();
 const path = require('path');
-
-// GET request to display notes.html
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'))
 })
-
-// GET request to display index.html
 router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'))
 })
