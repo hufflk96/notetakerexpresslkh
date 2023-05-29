56fc9e2c9e018310b8c93fcf6b439665cc7d7748
hufflk96 <118147274+hufflk96@users.noreply.github.com>
2023-05-29 08:01:39 -0400

    Update apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,5 @@
 const router = require('express').Router();
 const store = require('../db/store')
-
-
-// GET request for reading notes in db storage and return notes as json
 router.get('/notes', (req, res) => {
   store
     .getNotes()
@@ -11,7 +8,6 @@ router.get('/notes', (req, res) => {
     })
     .catch((err) => res.status(500).json(err));
 });
-// POST request for saving new note -> add to db storage -> return note to client
 router.post('/notes', (req, res) => {
   store
     .addNote(req.body)
@@ -19,7 +15,6 @@ router.post('/notes', (req, res) => {
     .catch((err) => res.status(500).json(err));
 
 });
-// **DELETE request to recieve id of note to delete -> read all notes from storage -> remove note with given id -> rewrite notes to db
 router.delete('/notes/:id', (req, res) => {
   store
     .deleteNote(req.params.id)
